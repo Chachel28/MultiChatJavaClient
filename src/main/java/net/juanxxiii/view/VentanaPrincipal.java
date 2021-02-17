59486b592525757038bc4476d7daff19921f68cb
Chachel28 <alexgb11@gmail.com>
2021-02-17 13:10:39 +0100

    Creado hilo para enviar mensaje, objeto de transferencia de datos mensaje, e inicio de logica para la conexion al servidor

--- src/main/java/net/juanxxiii/view/VentanaPrincipal.java
+++ src/main/java/net/juanxxiii/view/VentanaPrincipal.java
@@ -1,6 +1,8 @@
 package net.juanxxiii.view;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.net.Socket;
 
 public class VentanaPrincipal {
 
@@ -41,6 +43,11 @@ public class VentanaPrincipal {
             textField1.setEnabled(true);
             enviarButton.setEnabled(true);
             conectarseButton.setText("Desconectarse");
+            try {
+                Socket client = new Socket("localhost", 9090);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
